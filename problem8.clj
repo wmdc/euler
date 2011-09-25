f7a31d6b0e0f85d6e49405fe6b3a7d5b27a4fa07
Michael <mwelsmandinelle@gmail.com>
2011-09-25 04:28:40 -0700

    adding problem 8

---
+++ problem8.clj
@@ -0,0 +1,14 @@
+;Problem 8 - find the sum of digits in 100!
+
+(def factorial
+  (fn [n]
+    (loop [cnt n acc 1]
+       (if (zero? cnt)
+            acc
+          (recur (dec cnt) (* acc cnt))))))
+
+
+(let [x (atom 0)]
+	   (doseq [i (str (factorial 100))]
+		  (reset! x (+ @x (- (int i) (int \0)))))
+	   @x)
