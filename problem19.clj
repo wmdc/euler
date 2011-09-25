2a97128fe3e324d828cf8a75af390c44fde998da
Michael <mwelsmandinelle@gmail.com>
2011-09-25 00:42:14 -0700

    fix tabs

--- problem19.clj
+++ problem19.clj
@@ -9,23 +9,24 @@
 (defn divisible? [num den] (= 0 (rem num den)))
 
 (def days-in-month {:jan 31 :feb 28 :mar 31 
-					:apr 30 :may 31 :jun 30 
-					:jul 31 :aug 31 :sep 30 
-					:oct 31 :nov 30 :dec 31}
+                    :apr 30 :may 31 :jun 30
+                    :jul 31 :aug 31 :sep 30 
+                    :oct 31 :nov 30 :dec 31}
 					
 (defn leap-year? [year]
-	(cond 	(divisible? year 400) true
-			(divisible? year 100) false
-			(divisible? year 4) true
-			:else false))
+  (cond (divisible? year 400) true
+        (divisible? year 100) false
+        (divisible? year 4) true
+        :else false))
 
 (let [weekday (atom 0) total (atom 0)]
-	(doseq [year (range 1901 2001)]
-		(doseq [month months-in-year]
-			(doseq [day (range 1 (if (and (= month :feb) (leap-year? year))
-									(+ 2 (days-in-month month)) 
-									(+ 1 (days-in-month month))))]
-				(if (and (= day 1) (= @weekday 0)) (swap! total inc))
-				;(println (days-of-week @weekday) month day year)
-				(swap! weekday #(mod (inc %) days-per-week)))))
-	@total)
+  (doseq [year (range 1901 2001)]
+    (doseq [month months-in-year]
+      (doseq [day (range 1 (if  (and (= month :feb) (leap-year? year))
+                                (+ 2 (days-in-month month)) 
+                                (+ 1 (days-in-month month))))]
+        (if (and (= day 1) (= @weekday 0)) 
+          (swap! total inc))
+        ;(println (days-of-week @weekday) month day year)
+        (swap! weekday #(mod (inc %) days-per-week)))))
+  @total)
